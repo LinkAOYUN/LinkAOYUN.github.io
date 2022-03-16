8ae429ebc5612731383614a99b5036ae2dace224
LinkAOYUN <101641899+LinkAOYUN@users.noreply.github.com>
2022-03-16 20:02:25 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
     let targetImage = document.querySelector("#smart-image");
-    targetImage.classList.add("small");
+    targetImage.classList.add();
 targetImage.classList.remove("small");
 });
 });
